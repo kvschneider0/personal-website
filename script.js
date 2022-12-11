845089bbd9790817cbd84274aba6dcdf3612b89e
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2022-12-11 13:09:04 -0700

    minor bugs

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-import { generateLaplacianMatrix } from "./Thesis/matrix-generation";
+import { generateLaplacianMatrix } from "./matrix-generation";
 
 const button = document.getElementById('button');
 const output = document.getElementById('output');
-const countElement = document.getElementById('count');
+const countElement = document.getElementById('output');
 
 let countValue = 0;
 
@@ -11,8 +11,28 @@ function click() {
     countElement.innerHTML = `count = ${countValue}`;
 }
 
-function matrix() {
-    
+// function click() {
+//     const newHTML = matrixToHTML(generateLaplacianMatrix(5));
+//     output.innerHTML = newHTML;
+
+//     console.log('clicked');
+//     Alert('clicked');
+// }
+
+function matrixToHTML(matrix) {
+    const n = matrix.length;
+    let result = '<table>';
+
+    for (let i = 0; i < n; i++) {
+        result += '<tr>';
+        for (let j = 0; j < n; j++) {
+            result += `<td>${matrix[i][j]}</td>`;
+        }
+        result += '</tr>';
+    }
+    result += '</table>';
+
+    return result;
 }
 
 button.addEventListener('click', click);
--- test.js
+++ test.js
@@ -1,6 +1,25 @@
 import { generateLaplacianMatrix, printMatrix } from "./Thesis/matrix-generation.js";
 
-const mat = generateLaplacianMatrix(5);
+const mat = generateLaplacianMatrix(3);
+
+function matrixToHTML(matrix) {
+    const n = matrix.length;
+    let result = '<table>';
+
+    for (let i = 0; i < n; i++) {
+        result += '<tr>';
+        for (let j = 0; j < n; j++) {
+            result += `<td>${matrix[i][j]}</td>`;
+        }
+        result += '</tr>';
+    }
+    result += '</table>';
+
+    return result;
+
+}
+
+console.log(matrixToHTML(mat));
 
 printMatrix(mat);
 
