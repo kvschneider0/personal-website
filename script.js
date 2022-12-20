ed106d11d8cd9d743c158496d742ca7ee7d47e32
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2022-12-20 09:28:57 -0700

    added text to show matrix controllability

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 import { generateMatrix } from "./matrix.js";
 
 const button = document.getElementById('button');
-const output = document.getElementById('matrix-display');
+const mathOutput = document.getElementById('matrix-math-div');
+const controllabilityOutput = document.getElementById('matrix-controllability-div');
 
 function click() {
     const value = document.getElementById('dim');
     const dim = parseInt(value.value, 10);
     const [newHTML, controllability]  = generateMatrix(dim);
-    output.innerHTML = newHTML;
+    mathOutput.innerHTML = newHTML;
+    controllabilityOutput.innerHTML = `<em>${controllability}</em>`;
     MathJax.typeset();
 }
 click();
